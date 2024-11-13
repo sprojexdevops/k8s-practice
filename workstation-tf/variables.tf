85d70b8677b8ccfe2d72886703df885abfac41bc
yeswanth <yswnth.2010@gmail.com>
2024-11-13 18:36:01 +0530

    k8s practice

---
+++ workstation-tf/variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-xxxxxxxxxxxxx"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-xxxxxxxxxxx"
+}
