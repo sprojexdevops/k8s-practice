759a3f349acc9e4f84ce1ae043542910d3ef5217
yeswanth <yswnth.2010@gmail.com>
2025-06-12 21:39:32 +0530

    k8s practice

--- workstation-tf/workstation.sh
+++ workstation-tf/workstation.sh
@@ -60,7 +60,11 @@ VALIDATE $? "kubens installation"
 
 
 # Helm
-# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-# chmod 700 get_helm.sh
-# ./get_helm.sh
-# VALIDATE $? "helm installation"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+VALIDATE $? "helm installation"
+
+#k9s
+curl -sS https://webinstall.dev/k9s | bash
+VALIDATE $? "k9s installation"
