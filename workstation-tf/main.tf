d382f40a238018a3c387e850f3f04cf56e050101
yeswanth <yswnth.2010@gmail.com>
2025-06-21 18:03:33 +0530

    k8s practice

--- workstation-tf/main.tf
+++ workstation-tf/main.tf
@@ -7,7 +7,7 @@ module "workstation" {
   vpc_security_group_ids = [var.sg_id]
   # convert StringList to list and get first element
   subnet_id = var.public_subnet_id
-  ami = data.aws_ami.ami_info.id
+  ami = data.aws_ami.ami.id
   user_data = file("workstation.sh")
   # after completing, need to run aws configure after login to server
   tags = {
--- workstation-tf/variables.tf
+++ workstation-tf/variables.tf
@@ -1,10 +1,10 @@
 variable "sg_id" {
   type        = string
-  default     = "sg-xxxxxxxxxxxxx"
+  default     = "sg-0a1fb132f1fc1e49d"
   description = "description"
 }
 
 variable "public_subnet_id" {
     type = string
-    default = "subnet-xxxxxxxxxxx"
+    default = "subnet-08d3f59de08bd6000"
 }
